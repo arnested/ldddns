83613e8b4a7e8fb49c42729ba3365985aac7d7b5
Arne Jørgensen <arne@arnested.dk>
2021-01-02 00:09:10 +0100

    Fix name

--- service.go
+++ service.go
@@ -12,7 +12,7 @@ import (
 
 func installService() {
 	serviceUnit := `[Unit]
-Description=Docker Name Service
+Description=Local Docker Development DNS
 BindTo=docker.service
 After=docker.service
 
