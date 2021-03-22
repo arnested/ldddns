348c5fc86f759274ed7b9f7aae733509ce24d8dc
Arne Jørgensen <arne@arnested.dk>
2021-03-22 11:42:40 +0100

    Remove cleanup from early versions

--- deb/postinstall.sh
+++ deb/postinstall.sh
@@ -7,11 +7,6 @@ docker_version=$(/usr/bin/docker version --format '{{ .Server.APIVersion }}')
 mkdir --parents /usr/lib/systemd/system/ldddns.service.d
 printf "[Service]\nEnvironment=DOCKER_API_VERSION=%s\n" "${docker_version}" > /usr/lib/systemd/system/ldddns.service.d/docker-version.conf
 
-# Remove config file from previous versions.
-if [ -f /etc/default/ldddns ]; then
-    rm /etc/default/ldddns
-fi
-
 # Work around systemd reporting dropins being changed on disk.
 # See https://github.com/systemd/systemd/issues/17730
 # Fixed in 248-rc3 (https://github.com/systemd/systemd/pull/18869).
